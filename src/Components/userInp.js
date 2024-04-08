7a3a7de8cc0245b3b14920e28badf95db5dbc81c
Saurav Achari <123930348+saurav-04@users.noreply.github.com>
2024-04-08 18:52:29 +0530

    Create userInp.js

---
+++ src/Components/userInp.js
@@ -0,0 +1,9 @@
+ export const UserInp = () =>{
+    return(
+        <div className=" user-inp">
+            <span>
+               <input type='number' name='number' placeholder='0'/>
+            </span>
+        </div>
+    )
+ }
