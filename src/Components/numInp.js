086be6dc9c7a03db21a7d3e19bc7b3ab3f1f048e
Saurav Achari <123930348+saurav-04@users.noreply.github.com>
2024-04-08 19:05:36 +0530

    Update numInp.js

--- src/Components/numInp.js
+++ src/Components/numInp.js
@@ -1,16 +1,18 @@
 export const NumInp = ()=>{
     return(
-      <span>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-      </span>
+      <div className="num-inp">
+        <span>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+        </span>
+      </div>
 
     );
 }
