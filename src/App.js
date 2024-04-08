aef66802d2f92dd9fae46590b5c47bf283c71240
Saurav Achari <123930348+saurav-04@users.noreply.github.com>
2024-04-08 23:29:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import './App.css';
+import { useState } from 'react';
 import { UserInp } from './Components/userInp'
 import { NumInp } from './Components/numInp';
 import { OperatorInp } from './Components/operatorInp';
+import { CalcContext } from './Helpers/context';
 
 function App() {
+  const [ input , setInput ] = useState(0)
+  const [ result, setResult ] = useState(0)
+
+
+  
+
   return (
     <div className="App">
       
       <span><h1>CALCULATOR</h1></span>
-      <UserInp/>
-      <NumInp/>
-      <OperatorInp/>
+      
+      <CalcContext.Provider value={{input,setInput}}>
+          <UserInp/>
+          <NumInp/>
+          <OperatorInp/>
+      </CalcContext.Provider>
+
+      <span className='result'>
+        {result}
+      </span>
 
     </div>
   );
