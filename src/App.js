9b140990d299df4ca254fb2d40e6b26916e7239c
Saurav Achari <123930348+saurav-04@users.noreply.github.com>
2024-04-08 18:57:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import {UserInp} from './Components/userInp'
-
-
+import { NumInp } from './Components/numInp';
 
 function App() {
   return (
@@ -9,20 +8,7 @@ function App() {
       
       <span><h1>CALCULATOR</h1></span>
       <UserInp/>
-      
-
-      <span>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-      </span>
-
+      <NumInp/>
       <span>
         <button>+</button>
         <button>-</button>
