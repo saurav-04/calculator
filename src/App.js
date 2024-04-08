cb7b3cc89d54bfb64e9cd56b9b6c7647de8e1ef3
Saurav Achari <123930348+saurav-04@users.noreply.github.com>
2024-04-08 17:51:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,31 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      
+      <span><h1>CALCULATOR</h1></span>
+
+      <span>
+      <input type='number' name='number' placeholder='0'/>
+      </span>
+
+      <span>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+      </span>
+
+      <span>
+        <button>+</button>
+        <button>-</button>
+        <button>%</button>
+        <button>x</button>
+      </span>
 
     </div>
   );
