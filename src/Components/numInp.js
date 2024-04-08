6da7f01f92f38b34daf1a8ed61af249083b151c0
Saurav Achari <123930348+saurav-04@users.noreply.github.com>
2024-04-08 18:57:33 +0530

    Create numInp.js

---
+++ src/Components/numInp.js
@@ -0,0 +1,16 @@
+export const NumInp = ()=>{
+    return(
+      <span>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+      </span>
+
+    );
+}
