c3de0c274b6bb2b94ec57ba151d0e9df42e23ee1
Saurav Achari <123930348+saurav-04@users.noreply.github.com>
2024-04-08 19:05:40 +0530

    Create operatorInp.js

---
+++ src/Components/operatorInp.js
@@ -0,0 +1,12 @@
+export const OperatorInp = ()=>{
+    return(
+        <div className="operator-inp">
+            <span>
+                <button>+</button>
+                <button>-</button>
+                <button>%</button>
+                <button>x</button>
+            </span>
+        </div>
+    );
+}
